2abe073c58753c5bd779be3d12c0724f93fc666e
Hom Yan <yanzhihong23@qq.com>
2024-05-13 23:07:19 +0800

    chore: add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,17 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'eslint-config-prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint', 'unused-imports', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'unused-imports/no-unused-imports': 'error',
   },
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { Suspense, lazy } from "react";
+import { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const Home = lazy(() => import('./pages/Home'))
